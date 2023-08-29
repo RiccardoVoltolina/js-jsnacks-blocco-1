6f0a91bb95ab7f8a49abe179b3be9899168a193a
Riccardo <riky.volto@live.com>
2023-08-29 18:21:49 +0200

    bug html print

--- bonus/main.js
+++ bonus/main.js
@@ -3,14 +3,14 @@ Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
 
-
-
+let stampa = document.querySelector('div')
 let arrayVuoto = [];
 for (let i = 0; i < 6; i++) {
     const domanda = Number (prompt ('inserisci un numero'));
     if (domanda % 2 !== 0) {
         arrayVuoto.push (domanda)
         console.log(arrayVuoto);
+        stampa.innerHTML(arrayVuoto)
     } else {
         console.log('pari');
     }
